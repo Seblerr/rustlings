342c39e28b0ce3352a12689748bbcddef921c8e1
Sebastian <seblerr@gmail.com>
2023-03-07 08:18:07 +0100

    finished options

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -32,7 +32,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        basket.entry(fruit).or_insert(5)
+        basket.entry(fruit).or_insert(5);
 
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already

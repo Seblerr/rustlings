219d9e7fa06d8fef415f830e12c513288acc022e
Sebastian Nilsson <seblerr@gmail.com>
2023-05-15 23:01:24 +0200

    lifetimes and iterators

--- exercises/iterators/iterators3.rs
+++ exercises/iterators/iterators3.rs
@@ -23,7 +23,17 @@ pub struct NotDivisibleError {
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
-    todo!();
+    if b == 0 {
+        return Err(DivisionError::DivideByZero);
+    }
+    let result = (a / b, a % b);
+    if result.1 == 0 {
+        Ok::<i32, DivisionError>(result.0);
+    }
+    return Err(DivisionError::NotDivisible(NotDivisibleError {
+        dividend: a,
+        divisor: b,
+    }));
 }
 
 // Complete the function and return a value of the correct type so the test passes.
